11728cf65219d62e2c838aa924a4bed4c62df503
Ben Sinclair <ben@moopet.net>
2025-10-09 15:57:58 +0100

    Remove light mode

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,8 @@
-<script lang="ts">
-  import DarkModeToggle from '$lib/components/DarkModeToggle.svelte';
-</script>
-
 <header>
   <div class="home">
     <a aria-labelledby="home-navigation" href="/"><svg focusable="false" fill="currentColor" aria-hidden="true" viewBox="0 0 24 24"><path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z"></path></svg></a>
     <h1 id="home-navigation">Talker archive</h1>
   </div>
-
-  <DarkModeToggle />
 
   <nav>
     <a href="/resources">Resources</a>
