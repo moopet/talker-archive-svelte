2b47aa01cab9bb1323fc8aae3c595d12802f2d8a
Ben Sinclair <ben@moopet.net>
2025-10-14 13:01:54 +0100

    Add show-closed-talkers toggle to list page

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -5,9 +5,10 @@
   import { getActiveTalkers, getTalkerSlug } from '$lib/utils.ts';
   import { talkers } from '$lib/data/talkers.json';
 
-  let loading = $state(true);
-  let error = $state('');
+  let loading: boolean = $state(true);
+  let error: string = $state('');
   let lastCheckedDate = $state(null);
+  let showClosedTalkers: boolean = $state(false);
 
   let allTalkers = $state(talkers);
   let activeTalkers = $state([]);
@@ -67,6 +68,13 @@
     <p>Last update: {formatDistanceToNow(lastCheckedDate, { addSuffix: true })}</p>
   {/if}
 
+  <fieldset>
+    <div class="show-closed-talkers">
+      <input type="checkbox" bind:checked={showClosedTalkers} id="show-closed-talkers-toggle" />
+      <label for="show-closed-talkers-toggle"><span>show closed talkers</span></label>
+    </div>
+  </fieldset>
+
   <table>
     <thead>
       <tr>
@@ -78,31 +86,33 @@
 
     <tbody>
       {#each allTalkers as talker}
-        <tr>
-          <td><a href={`/details/${getTalkerSlug(talker)}`}>{talker.name}</a></td>
+        {#if showClosedTalkers || !talker.isClosed}
+          <tr>
+            <td><a href={`/details/${getTalkerSlug(talker)}`}>{talker.name}</a></td>
 
-          <td>
-            {#if talker.isActive}
-              <a href="telnet://{talker.hostname}:{talker.port}">{talker.hostname}:{talker.port}</a>
-            {:else}
-              <span>n/a</span>
-            {/if}
-          </td>
-
-          {#if loading}
-              <td class="status-loading">...</td>
-          {:else}
-            {#if talker.isActive}
-              <td class="status-up">UP</td>
-            {:else}
-              {#if talker.isClosed}
-                <td class="status-closed">CLOSED</td>
+            <td>
+              {#if talker.isActive}
+                <a href="telnet://{talker.hostname}:{talker.port}">{talker.hostname}:{talker.port}</a>
               {:else}
-                <td class="status-down">DOWN</td>
+                <span>n/a</span>
+              {/if}
+            </td>
+
+            {#if loading}
+                <td class="status-loading">...</td>
+            {:else}
+              {#if talker.isActive}
+                <td class="status-up">UP</td>
+              {:else}
+                {#if talker.isClosed}
+                  <td class="status-closed">CLOSED</td>
+                {:else}
+                  <td class="status-down">DOWN</td>
+                {/if}
               {/if}
             {/if}
-          {/if}
-        </tr>
+          </tr>
+        {/if}
       {/each}
     </tbody>
   </table>
@@ -139,5 +149,84 @@
 
   td span {
     opacity: 0.3;
+  }
+
+  fieldset {
+    padding-inline-start: 2rem;
+    padding-inline-end: 2rem;
+    padding-block-start: 2rem;
+    padding-block-end: 2rem;
+    border-width: 0;
+    background-color: var(--card-background-color);
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
+  [type=checkbox] {
+    opacity: 0;
+	  height: 0;
+	  width: 0;
+  }
+
+  .show-closed-talkers:focus-within {
+    outline: medium auto currentColor;
+  }
+
+  label[for=show-closed-talkers-toggle] {
+	  cursor: pointer;
+	  width: 40px;
+	  height: 16px;
+	  background-color: var(--toggle-track-color-off);
+	  display: block;
+	  border-radius: 25px;
+	  position: relative;
+    margin-inline-start: 16rem;
+    margin-block-start: 1rem;
+    text-indent: -16rem;
+
+    span {
+      position: absolute;
+      right: 0;
+      top: -4px;
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      top: -2px;
+      left: 0;
+      width: 20px;
+      height: 20px;
+      background-color: var(--toggle-knob-color-off);
+      border-radius: 25px;
+      transition: 0.3s;
+    }
+  }
+
+  input:checked + label {
+	  background-color: var(--toggle-track-color-on);
+  }
+
+  input:checked + label:after {
+	  left: calc(100% + 3px);
+	  background-color: var(--toggle-knob-color-on);
+	  transform: translateX(-100%);
+  }
+
+  .show-closed-talkers {
+    display: flex;
+    flex-direction: column;
+  }
+
+  @media (min-width: 660px) {
+    fieldset {
+      flex-direction: row;
+    }
+
+    label[for=show-closed-talkers-toggle] {
+      margin-inline-start: 0;
+      margin-block-start: 0;
+    }
   }
 </style>
