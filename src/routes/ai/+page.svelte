fe2f85cda319205474799e899d7af533494397d2
Ben Sinclair <ben@moopet.net>
2025-10-23 13:32:32 +0100

    Add slashai route

---
+++ src/routes/ai/+page.svelte
@@ -0,0 +1,21 @@
+<section>
+  <h1>AI use in this project</h1>
+
+  <p>I haven't directly used generative AI working on this project.</p>
+
+  <p>I don't use AI-assistance in my code editor. I've tried it in the past, and it's not for me. The benefits are not there yet - I'd spend more time correcting and debugging code produced by an LLM than I would just writing it in the first place.</p>
+
+  <p>I do occasionally use AI to answer questions where I can't find a good explanation elsewhere or I want to see some comparative points between two libraries, for example.</p>
+
+  <p>And that's about it.</p>
+</section>
+
+<style>
+h1 {
+  margin-block-end: 3rem;
+}
+
+section {
+  text-align: left;
+}
+</style>
