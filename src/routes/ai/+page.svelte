b8d04e45e8da34ecb80838412affa2f55bb4ce9d
Ben Sinclair <ben@moopet.net>
2025-10-23 13:47:15 +0100

    Add more to slashai page

--- src/routes/ai/+page.svelte
+++ src/routes/ai/+page.svelte
@@ -1,15 +1,39 @@
 <section>
   <h1>AI use in this project</h1>
 
-  <p>I haven't directly used generative AI working on this project.</p>
+  <h2>General disclaimer</h2>
 
-  <p>I don't use AI-assistance in my code editor. I've tried it in the past, and it's not for me. The benefits are not there yet - I'd spend more time correcting and debugging code produced by an LLM than I would just writing it in the first place.</p>
+  <p>
+    I haven't directly used generative AI working on this project.
+  </p>
 
-  <p>I do occasionally use AI to answer questions where I can't find a good explanation elsewhere or I want to see some comparative points between two libraries, for example.</p>
+  <p>
+    I don't use AI to write any copy, to summarise any text or to give me code. I like my own words too much.
+  </p>
 
-  <p>And that's about it.</p>
+  <p>
+    I don't use AI-assistance in my code editor. I've tried it in the past, and it's not for me. The benefits are not there yet - I'd spend more time correcting and debugging code produced by an LLM than I would just writing it in the first place.
+  </p>
+
+  <p>
+    I do occasionally use AI chat sessions to answer questions where I can't find a good explanation elsewhere or I want to see some comparative points between two libraries, for example.
+  </p>
+
+  <h2>Things I've tried out of curiosity</h2>
+
+  <p>
+    I've tried using AI to scrape some of the big lists of talkers I've found and convert the data into my preferred formats, but it's frustrating and shows exactly where context in LLMs is still attrocious.<br>
+    Theoretically it could have saved a lot of time, but in reality I'd be so suspicious of the output I'd never be happy.
+  </p>
+
+  <p>
+    I've tried using AI OCR to convert screenshots of login screens to text.<br>
+    The results were funny, but not surprising: either missing great chunks or recognising it as ASCII art and replacing chunks with its own stolen pictures of cats.
+  </p>
 
   <aside>
+    <p>And that's about it.</p>
+
     <p>
       If you're wondering why this page/disclaimer exists, read the <a href="https://www.bydamo.la/p/ai-manifesto">/ai 'manifesto'</a>.
     </p>
